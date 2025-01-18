ebbbccfd07544287028d9fa49253cb1147d5cd75
micky mack <jijomonjs@gmail.com>
2025-01-18 12:30:33 +0530

    connected mongoatlas

---
+++ config/multer-config.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinary');
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'Virgo_images', 
+        format: async (req, file) => file.mimetype.split('/')[1],
+        public_id: (req, file) => file.originalname.split('.')[0],
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 }, 
+    fileFilter: (req, file, cb) => {
+        if (!file.mimetype.startsWith('image/')) {
+            return cb(new Error('Only image files are allowed!'), false);
+        }
+        cb(null, true);
+    },
+});
+
+module.exports = upload;
